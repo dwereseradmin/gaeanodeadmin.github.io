a4684a7653dcaf1ca42bc7eff6c09c7d49196bc2
gaea3 <develop@aigaea.net>
2025-07-23 03:12:00 +0000

    update 2025-07-23 03:12:00

---
+++ static/router-login.9f7fef14.js
@@ -0,0 +1,1 @@
+import{b_ as o,a0 as e}from"./entry.a9e81fe4.js";import{u as t}from"./user.ba6dc408.js";const u=o((r,s)=>{if(t().getToken){if(r.path==="/")return e("/home")}else return e("/login")});export{u as default};
