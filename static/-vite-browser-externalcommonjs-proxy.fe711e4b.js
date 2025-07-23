b0c804956d6dad18cb41b5c063c452a42a06acb5
gaea3 <develop@aigaea.net>
2025-07-23 03:45:44 +0000

    update 2025-07-23 03:45:44

---
+++ static/-vite-browser-externalcommonjs-proxy.fe711e4b.js
@@ -0,0 +1,1 @@
+import{a8 as e}from"./el-button.2c725bd2.js";const t={},r=Object.freeze(Object.defineProperty({__proto__:null,default:t},Symbol.toStringTag,{value:"Module"})),a=e(r);export{a as r};
---
+++ static/router-login.24bc045e.js
@@ -0,0 +1,1 @@
+import{b_ as o,a0 as e}from"./entry.fb5c69a9.js";import{u as t}from"./user.4854ed54.js";const u=o((r,s)=>{if(t().getToken){if(r.path==="/")return e("/home")}else return e("/login")});export{u as default};
