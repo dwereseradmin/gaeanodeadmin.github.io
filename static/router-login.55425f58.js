bb9c924561949d5598e01c9342ed95161b4ea1b1
gaea3 <develop@aigaea.net>
2025-09-03 04:59:12 +0000

    update 2025-09-03 04:59:12

---
+++ static/router-login.55425f58.js
@@ -0,0 +1,1 @@
+import{b_ as o,a0 as e}from"./entry.d37c3a0c.js";import{u as t}from"./user.acb79d4a.js";const u=o((r,s)=>{if(t().getToken){if(r.path==="/")return e("/home")}else return e("/login")});export{u as default};
